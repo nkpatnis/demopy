d2af402c5b049b8c79a8f03a7c74b51bb5309417
nikhil <nkpatnis@gmail.com>
2016-02-24 17:23:12 +0530

    test

--- classes/samples/API_intigration.py
+++ classes/samples/API_intigration.py
@@ -2,15 +2,17 @@ __author__ = 'nikhil'
 
 import http.client
 
-conn = http.client.HTTPSConnection("www.python.org")
+
+# non working code
+conn = http.client.HTTPSConnection("openweathermap.org")
 conn.request("GET", "/")
 r1 = conn.getresponse()
-print(r1.status, r1.reason)
+print(r1.status, r1.value)
 # 200 OK
-data1 = r1.read()
-conn.request("GET", "/")
-r2 = conn.getresponse()
-print(r2.status, r2.reason)
+# data1 = r1.read()
+# conn.request("GET", "/data/2.5/weather?q=London&appid=44db6a862fba0b067b1930da0d769e98")
+# r2 = conn.getresponse()
+# print(r2.status, r2.reason)
 # 404 Not Found
-data2 = r2.read()
+# data2 = r2.read()
 conn.close()
